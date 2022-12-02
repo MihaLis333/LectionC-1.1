599b7324c417d9982f56d5b4ebcca2603a95ae63
MihaLis333 <m190333@ya.ru>
2022-12-02 15:23:47 +0300

    составление примера работы с массивом

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int [] array = {2, 5, 4, 91, 21, 4, 65, 58, 99};
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+
+    index++;
+}
+
+
+
